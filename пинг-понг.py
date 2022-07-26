808e2cea2bc5f1cdb556b22d3260b098947ff428
sofiaPirozhkova <109730471+sofiaPirozhkova@users.noreply.github.com>
2022-07-26 13:00:21 +0300

    Add files via upload
    v1.0

---
+++ пинг-понг.py
@@ -0,0 +1,57 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys [K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys [K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+back = (200, 255, 255)
+
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOST', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOST', True, (180, 0, 0))
+
+
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+
+    display.update()
+    clock.tick(FPS)
